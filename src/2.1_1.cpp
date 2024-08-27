086c1292bdb835d6e46188b05f1b07b6e5937980
ouyangzhaoxin <1273730703@qq.com>
2024-08-27 22:07:45 +0800

    update src/2.1_1.cpp

--- src/2.1_1.cpp
+++ src/2.1_1.cpp
@@ -1,6 +1,9 @@
 //
 // Created by xin on 24-8-27.
-//
+// 前线程的函数已返回，新线程却仍能访问其局部变量
+// 以下做法极不可取：意图在函数中创建线程(比如，在oops()函数中创建线程，my_thread)，
+// 并让线程访问(oops)函数的局部变量(比如，my_thread线程访问局部变量some_local_state)
+
 #include <iostream>
 #include <thread>
 using namespace std;
@@ -33,8 +36,8 @@ void oops()
     int some_local_state = 0;
     func my_func(some_local_state);
     std::thread my_thread(my_func);
-    // my_thread.detach();
-    my_thread.join();
+    my_thread.detach();
+    // my_thread.join();
 }
 
 
