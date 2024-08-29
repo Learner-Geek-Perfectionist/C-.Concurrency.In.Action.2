3f8943c71425782448e7aa11021d85a3f7573682
ouyangzhaoxin <1273730703@qq.com>
2024-08-29 23:57:55 +0800

    update 2.1.3_2(_3).cpp

--- CPlusPlus_Multithreading_Examples/2.2_1.cpp
+++ CPlusPlus_Multithreading_Examples/2.2_1.cpp
@@ -9,20 +9,21 @@ using namespace std;
 
 void f(int i, string const& s)
 {
-    std::this_thread::sleep_for(std::chrono::seconds(3)); // 模拟耗时操作
+    // std::this_thread::sleep_for(std::chrono::seconds(3)); // 模拟耗时操作
     cout << i << endl << s << endl;
 }
 
 void oops(int some_param)
 {
-    char buffer[1024];
-    sprintf(buffer, "%i", some_param);
-    thread t(f, 3, (buffer)); // 传递参数的时候使用了函数的局部变量。
+    // char buffer[1024];
+    // sprintf(buffer, "%i", some_param);
+    auto sp = make_shared<string>(to_string(some_param));
+    thread t(f, 1, sp); // 传递参数的时候使用了函数的局部变量。
     t.detach();
 }
 
 int main()
 {
-    oops(123);
-    std::this_thread::sleep_for(std::chrono::seconds(5)); // 模拟耗时操作
+    oops(123456);
+    // std::this_thread::sleep_for(std::chrono::seconds(2)); // 模拟耗时操作
 }
