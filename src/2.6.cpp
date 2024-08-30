004e7bf1d697ee6dc83c815ac53d2ee21b883893
ouyangzhaoxin <1273730703@qq.com>
2024-08-31 03:03:32 +0800

    update 2.6.cpp

--- src/2.6.cpp
+++ src/2.6.cpp
@@ -1,16 +1,16 @@
 //
-// Created by xin on 24-8-28.
-// 利用 RAII 过程等待线程完结
+// Created by xin on 24-8-31.
+//
 
+#include <memory>
 #include <iostream>
 #include <thread>
-#include <chrono>
 using namespace std;
 
 void do_something(int i)
 {
     if (i == 0) cout << "success\n";
-    else cout << "failure\n";
+    else cout << i << ": failure\n";
 }
 
 void do_something_in_current_thread()
@@ -40,7 +40,7 @@ class thread_guard
     std::thread& t;
 
 public:
-    explicit thread_guard(std::thread& t_): t(t_)
+    explicit thread_guard(std::thread& t_) : t(t_)
     {
     }
 
@@ -49,17 +49,30 @@ public:
         if (t.joinable()) t.join();
     }
 
-    // 线程不能被拷贝，因此拷贝构造函数和拷贝赋值运算符被删除。
     thread_guard(thread_guard const&) = delete;
     thread_guard& operator=(thread_guard const&) = delete;
 };
 
+
+class scoped_thread
+{
+    thread t;
+
+public:
+    explicit scoped_thread(thread _t): t(move(_t))
+    {
+        if (!t.joinable()) throw logic_error("No thread");
+    }
+
+    ~scoped_thread() { t.join(); }
+    scoped_thread(scoped_thread const&) = delete;
+    scoped_thread& operator=(scoped_thread const&) = delete;
+};
+
 void f()
 {
-    int some_local_state = 0;
-    func my_func(some_local_state);
-    std::thread t(my_func);
-    thread_guard g(t);
+    int some_local_state;
+    scoped_thread t{std::thread(func(some_local_state))};
     do_something_in_current_thread();
 }
 
